16ea76eaad4985564fbad38b48b82653d6537df8
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2024-11-06 06:05:09 -0300

    refactor(errors): improve errors from update meeting schema

--- src/modules/meetings/schemas/update-transcript.schema.ts
+++ src/modules/meetings/schemas/update-transcript.schema.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const updateTranscriptSchema = z.object({
 	transcript: z
-		.string({ message: "The Transcription needs to be a text" })
-		.min(1, "A transcription needs to be provided"),
+		.string({ message: "Transcription needs to be a text" })
+		.min(1, "Transcription needs to be provided"),
 });
 
 export type UpdateTranscriptParams = z.infer<typeof updateTranscriptSchema>;
