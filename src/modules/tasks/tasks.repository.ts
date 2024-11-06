eb5fd351036b0e2c6ad7811704d867553af84815
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2024-11-06 14:38:08 -0300

    feat(repositories): add method to get tasks by meeting id

--- src/modules/tasks/tasks.repository.ts
+++ src/modules/tasks/tasks.repository.ts
@@ -1,6 +1,10 @@
 import { Task, type ITask } from "./models";
 
 export class TaskRepository {
+	async getByMeetingId(meetingId: string): Promise<ITask[]> {
+		return await Task.find({ meetingId }).lean().exec();
+	}
+
 	async getByUserId(userId: string): Promise<ITask[]> {
 		return await Task.find({ userId }).lean().exec();
 	}
