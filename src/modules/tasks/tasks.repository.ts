db3d749ed39f24cc98f592e5b8373bcb362ecb76
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2024-11-06 14:40:41 -0300

    feat(repositories): add method to get overdue tasks

--- src/modules/tasks/tasks.repository.ts
+++ src/modules/tasks/tasks.repository.ts
@@ -8,4 +8,39 @@ export class TaskRepository {
 	async getByUserId(userId: string): Promise<ITask[]> {
 		return await Task.find({ userId }).lean().exec();
 	}
+
+	async getOverdueTasks(userId: string) {
+		return await Task.aggregate([
+			{
+				$match: {
+					userId,
+					dueDate: { $lt: new Date() },
+					status: { $ne: "completed" },
+				},
+			},
+			{
+				$lookup: {
+					from: "meetings",
+					localField: "meetingId",
+					foreignField: "_id",
+					as: "meeting",
+				},
+			},
+			{ $unwind: "$meeting" },
+			{
+				$project: {
+					_id: 1,
+					title: 1,
+					dueDate: 1,
+					meetingId: "$meeting._id",
+					meetingTitle: "$meeting.title",
+				},
+			},
+			{
+				$sort: {
+					dueDate: 1,
+				},
+			},
+		]);
+	}
 }
