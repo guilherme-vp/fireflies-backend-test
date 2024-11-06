d58d9ca6bc0faae95b37f9b7b78937b40f7483b0
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2024-11-06 06:04:59 -0300

    refactor(errors): improve errors from create meeting schema

--- src/modules/meetings/schemas/create-meeting.schema.ts
+++ src/modules/meetings/schemas/create-meeting.schema.ts
@@ -1,15 +1,21 @@
 import { z } from "zod";
 
 export const createMeetingSchema = z.object({
-	title: z.string().min(1, "Title is required"),
+	title: z
+		.string({ message: "Title needs to be provided" })
+		.min(1, "Title is required"),
 	// Validates if the date is an ISO string
 	date: z
-		.string()
+		.string({ message: "Date needs to be provided" })
 		.refine((value) => !Number.isNaN(Date.parse(value)), {
 			message: "Date must be a valid ISO string",
 		})
 		.transform((value) => new Date(value)),
-	participants: z.array(z.string().min(1, "Participant name cannot be empty")),
+	participants: z
+		.array(z.string().min(1, "Participant name cannot be empty"), {
+			message: "Participants needs to be provided",
+		})
+		.min(1, "The meeting must have at least one participant name"),
 });
 
 export type CreateMeetingParams = z.infer<typeof createMeetingSchema>;
