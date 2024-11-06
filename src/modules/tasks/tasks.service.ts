8c54b5f600a56d017af8db2af482dfac4d0bef61
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2024-11-06 03:57:28 -0300

    feat(services): add tasks service

---
+++ src/modules/tasks/tasks.service.ts
@@ -0,0 +1,12 @@
+import { logger } from "../../utils";
+import type { TaskRepository } from "./tasks.repository";
+
+export class TasksService {
+	constructor(private readonly taskRepository: TaskRepository) {}
+
+	public async getUserTasks(userId: string) {
+		const tasks = await this.taskRepository.getTasksByUserId(userId);
+		logger.info("Found tasks for user", { userId });
+		return tasks;
+	}
+}
