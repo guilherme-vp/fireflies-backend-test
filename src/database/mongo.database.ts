e0cbf704f9888b6579fa1edfeb99144cfb249e16
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2024-11-07 00:57:47 -0300

    fix: wait for mongodb connection before starting express

--- src/database/mongo.database.ts
+++ src/database/mongo.database.ts
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 import { settings } from "../config";
 import { logger } from "../utils";
 
-mongoose.connect(`${settings.mongo.url}/${settings.mongo.database}`);
-
-mongoose.connection.on("connected", () => {
-	logger.info("Connected to MongoDB");
-});
-
-mongoose.connection.on("error", (err: unknown) => {
-	logger.error(`Error connecting to MongoDB: ${err}`);
-});
+export async function connectMongoDB() {
+	try {
+		await mongoose.connect(`${settings.mongo.url}/${settings.mongo.database}`);
+		logger.info("Connected to MongoDB");
+	} catch (err) {
+		logger.error("Error connecting to MongoDB. Exiting...", err);
+		process.exit(1);
+	}
+}
 
 export default mongoose;
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import expressApp from "./app";
 import { settings } from "./config";
 import { logger } from "./utils";
+import { connectMongoDB } from "./database";
 
-expressApp.on("ready", () => {
+// Initiates all databases before starting the Express server
+connectMongoDB().then(() => {
 	expressApp.listen(settings.app.port, async () => {
 		logger.info(`Server is running on port ${settings.app.port}`);
 	});
-});
-
-// Initiates all databases before starting the Express server
-await import("./database").then(() => {
-	expressApp.emit("ready");
 });
